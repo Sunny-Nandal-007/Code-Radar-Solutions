55e7336eb6b3ec31c6efe78c871c9f71d98b8827
Sunny-Nandal-007 <sunny1702.becse24@chitkara.edu.in>
2025-02-18 02:00:23 +0530

    Solution to Check Multiples

---
+++ Check Multiples@@6788dd63490637cfc44e1b9a/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num,divisor;
+
+    scanf("%d %d", &num,&divisor);
+
+    if () {
+        printf("Yes");
+    } else {
+        printf("No");
+    }
+
+    return 0;
+}
