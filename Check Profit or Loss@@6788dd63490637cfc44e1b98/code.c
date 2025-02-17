efe062baa17d26325ca26d1344a4612647f84c41
Sunny-Nandal-007 <sunny1702.becse24@chitkara.edu.in>
2025-02-18 01:48:12 +0530

    Solution to Check Profit or Loss

---
+++ Check Profit or Loss@@6788dd63490637cfc44e1b98/code.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main() {
+    
+    if () {
+        printf("Profit");
+    }
+    else if () {
+        printf("Loss");
+    
+    }
+    else {
+        printf("No Profit No Loss"); 
+    }
+
+    return 0;
+}
