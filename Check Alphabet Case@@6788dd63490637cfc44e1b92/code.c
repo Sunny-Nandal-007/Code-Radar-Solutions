73508ca3455f89c79c787ad263fb028a6f66d67b
Sunny-Nandal-007 <sunny1702.becse24@chitkara.edu.in>
2025-02-17 23:51:30 +0530

    Solution to Check Alphabet Case

---
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {   
+    char alphabet
+    \;
+    scanf("%d",&year);
+     if ('A'<= alphabet <= 'Z'){
+        printf("Uppercase");
+    }    
+    else{
+        printf("Lowercase");
+    }
+    return 0;
+}
